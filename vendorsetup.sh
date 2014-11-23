bb68f58f45d2ec1d7ad3d6af75e73543dbfc3dba
martin coulon <martinusbe@gmail.com>
2014-11-23 19:38:42 +0100

    add hercules

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ add_lunch_combo validus_d2vzw-userdebug
 add_lunch_combo validus_d851-userdebug
 add_lunch_combo validus_falcon-userdebug
 add_lunch_combo validus_hammerhead-userdebug
+add_lunch_combo validus_hercules-userdebug
 add_lunch_combo validus_ghost-userdebug
 add_lunch_combo validus_m8-userdebug
 add_lunch_combo validus_mako-userdebug
