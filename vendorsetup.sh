c351d07e7ed3fa14c00107305f62e4dfda50756d
martin coulon <martinusbe@gmail.com>
2014-11-28 17:22:55 +0100

    d2att

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo validus_bacon-userdebug
 add_lunch_combo validus_d2vzw-userdebug
+add_lunch_combo validus_d2att-userdebug
 add_lunch_combo validus_d851-userdebug
 add_lunch_combo validus_falcon-userdebug
 add_lunch_combo validus_hammerhead-userdebug
