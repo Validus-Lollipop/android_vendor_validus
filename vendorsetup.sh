0df15752158dd9ce37b4212b2d3e221e4030ac95
Randy DuCharme <radio.ad5gb@gmail.com>
2014-11-30 14:04:23 -0600

    Update vendorsetup.sh
    Add d852

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ add_lunch_combo validus_d2att-userdebug
 add_lunch_combo validus_d2tmo-userdebug
 add_lunch_combo validus_d2vzw-userdebug
 add_lunch_combo validus_d851-userdebug
+add_lunch_combo validus_d852-userdebug
 add_lunch_combo validus_falcon-userdebug
 add_lunch_combo validus_hammerhead-userdebug
 add_lunch_combo validus_hercules-userdebug
